a0d90d46912b58deb098366f147e5f96aa798a4e
Moses <asugam9@gmail.com>
2024-11-25 21:22:50 +0300

    pug

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express') 
+import express from "express";
 
 const app = express()
 
-PORT = 3000;
+app.set("view engine", "pug");
+app.set("views" , "./views");
+
+
+const PORT = 3000;
+app.use(express.static('./views/style.css'));
+app.use(express.json());
 
 app.get('/',(req,res) =>{
-res.send('hello world')
+res.render('login')
+
+});
+app.post('/login', (req,res) =>{    
+    const {username,email,password} = req.body
+    console.log(req.body);
 });
 
 
